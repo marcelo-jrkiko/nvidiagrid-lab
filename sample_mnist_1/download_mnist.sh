a6e2ee9847c73bc36a69088f7ff9694a9ca7d06b
Kiko <marcelojunior.tech@gmail.com>
2026-04-03 16:36:53 -0300

    patch: Download DATA from Archived Repo

--- sample_mnist_1/download_mnist.sh
+++ sample_mnist_1/download_mnist.sh
@@ -4,31 +4,8 @@
 set -e
 
 echo "Downloading MNIST dataset..."
-
-# Create data directory
-mkdir -p mnist_data
+git clone https://github.com/marcelo-jrkiko/mnist_repo.git mnist_data
 cd mnist_data
-
-# Download MNIST dataset
-if [ ! -f train-images-idx3-ubyte.gz ]; then
-    echo "Downloading MNIST training images..."
-    wget -q http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
-fi
-
-if [ ! -f train-labels-idx1-ubyte.gz ]; then
-    echo "Downloading MNIST training labels..."
-    wget -q http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
-fi
-
-if [ ! -f t10k-images-idx3-ubyte.gz ]; then
-    echo "Downloading MNIST test images..."
-    wget -q http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
-fi
-
-if [ ! -f t10k-labels-idx1-ubyte.gz ]; then
-    echo "Downloading MNIST test labels..."
-    wget -q http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
-fi
 
 # Extract
 echo "Extracting..."
